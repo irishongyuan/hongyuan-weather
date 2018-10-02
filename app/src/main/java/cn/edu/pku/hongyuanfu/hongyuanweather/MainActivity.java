707971c292caa065bd8cbf08a8f2d1b9d63ff162
hongyuanfu <1771593876@qq.com>
2018-10-02 11:58:54 +0800

    first commit

---
+++ app/src/main/java/cn/edu/pku/hongyuanfu/hongyuanweather/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.pku.hongyuanfu.hongyuanweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
